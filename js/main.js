b2b0a026514ac1344ba1a45029e58a33fd36ac28
Karla Valeria Rodriguez <valartist554@gmail.com>
2024-08-06 17:09:29 -0700

    mobile contact styles

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "flex") {
         x.style.display = "none";
+        var y = document.getElementById("body");
+        y.style.overflow = "scroll";
     } else {
         x.style.display = "flex";
         var y = document.getElementById("body");
